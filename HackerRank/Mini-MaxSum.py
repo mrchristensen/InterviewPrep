310b580895d97b86cfc46a1228b9347549280cdc
Matt Christensen <mr.christensen@gmail.com>
2022-06-26 22:45:31 -0600

    Mini-Max Sum

---
+++ HackerRank/Mini-MaxSum.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    min = arr[0]
+    max = arr[0]
+    sum = 0
+    
+    for num in arr:
+        sum += num
+        
+        if(num < min):
+            min = num
+        elif(num > max):
+            max = num
+            
+    print(str(sum - max) + " " + str(sum - min))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
