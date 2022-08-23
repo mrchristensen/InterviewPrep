0382c97cf546ea9462da92d85993844fdad12835
Matt Christensen <mr.christensen@gmail.com>
2022-08-23 16:13:18 -0600

    Create TwoSum.py

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/two-sum/
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index = 0
+        complements = {}
+        
+        for num in nums:
+            if(num in complements):
+                return [complements[num], index]
+            else:
+                complements[target - num] = index
+            
+            index += 1
