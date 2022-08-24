aa07e21810ebf87b2ccf98bbfdc228fb866a6a7f
Matt Christensen <mr.christensen@gmail.com>
2022-08-24 16:07:42 -0600

    Create PowerOfThree.py

---
+++ LeetCode/PowerOfThree.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-three/
+
+# Time Complexity: O(?) (whatever the math.log10() function costs)
+# Space Complexity: O(1)
+
+import math
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if(n <= 0):
+            return False
+        
+        return (math.log10(n)/math.log10(3)) % 1 == 0
