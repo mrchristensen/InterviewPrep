858dd853d30d9ae189788e87464a9bd53cad4cb7
Matt Christensen <mr.christensen@gmail.com>
2022-08-25 11:53:30 -0600

    Create RomanToInteger.py

---
+++ RomanToInteger.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/roman-to-integer/submissions/
+
+# 21 min
+
+# Time Complexity: O(n) (where n is number of digits)
+# Space Complexity: O(1)
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        end_of_string = 0
+        total = 0
+        length = len(s)
+        
+        for i in range(length):
+            cur_value = roman_map[s[i]]
+            prox_value = roman_map[s[i + 1]] if i + 1 < length else end_of_string
+            
+            if(cur_value >= prox_value):
+                total += cur_value
+            else:
+                total -= cur_value
+                
+        return total
+    
