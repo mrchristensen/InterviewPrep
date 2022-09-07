9bc32b29bb487fecc8bf6120f9bbc620feb461e4
Matt Christensen <mr.christensen@gmail.com>
2022-09-07 16:58:32 -0600

    Create IsomorphicStrings.py

---
+++ LeetCode/IsomorphicStrings.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/isomorphic-strings/submissions
+# Runtime: 62 ms, faster than 59.36% of Python3 online submissions for Isomorphic Strings.
+# Memory Usage: 14.1 MB, less than 88.68% of Python3 online submissions for Isomorphic Strings.
+
+# 20 minites
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping = {}
+        
+        for i in range(len(s)):            
+            if(s[i] not in mapping):
+                if(mapping.items and t[i] in mapping.values()):
+                    return False
+                mapping[s[i]] = t[i]
+                
+            if(mapping[s[i]] != t[i]):
+                return False
+        
+        return True
+        
