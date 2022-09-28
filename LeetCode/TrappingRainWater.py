07baa33a3f227411f29676cf13e38f27ef987ef0
Matt Christensen <mr.christensen@gmail.com>
2022-09-28 13:14:26 -0600

    Added older solution (comment out below)

--- LeetCode/TrappingRainWater.py
+++ LeetCode/TrappingRainWater.py
@@ -112,3 +112,58 @@ class Solution:
 #             node = node.next_node
 
 #         return agua
+
+
+# Old solution (doesn't work, fails some test cases
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         optima_1, optima_2, agua, water_level_to_tallest_point_tiles = 0, 0, 0, 0
+#         valley_tiles, valley_height = 0, 0
+#         tallest_point = 0
+        
+#         print("height before:", height)
+        
+#         # For bounds
+#         height.insert(0, 0)
+#         height.append(0)
+
+#         print("height after:", height)
+
+#         for i in range(1, len(height) - 1):
+#             print("i:", i, "height[i]:", height[i])
+#             # If we find a local optima
+#             point = height[i]
+#             peaks = []
+            
+#             if(point >= height[i - 1] and point >= height[i + 1]):
+                
+#                 print("found a peak")                             
+#                 optima_1 = optima_2
+#                 optima_2 = height[i]
+#                 # Water line at the lowest of the two peaks
+#                 water_line = min(optima_1, optima_2)
+                
+#                 agua += (water_line * valley_tiles) - valley_height
+                
+#                 # If we find that our local optima it in a valley between two peak, we'll want the water
+#                 # +1 for previous peak that is now part of a valley (and thus not previously counted)
+#                 water_level_to_tallest_point_tiles += (tallest_point - water_line) * (valley_tiles)
+#                 valley_tiles, valley_height = 0, 0
+                
+#                 # If this local optima is the tallest peak we've seen so far
+#                 if(point > tallest_point):
+#                     print("We found a new tallest point")
+#                     print("Adding:", water_level_to_tallest_point_tiles, "to water")
+#                     # Move water line to previous tallest point
+#                     agua += water_level_to_tallest_point_tiles
+#                     water_level_to_tallest_point_tiles = 0
+                    
+#                     # Update tallest point
+#                     tallest_point = point
+#                 # else:
+#                 #     pass
+#             else:
+#                 valley_tiles += 1
+#                 valley_height += height[i]
+                
+#         return agua
